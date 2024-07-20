e776f23ab832a4785c56c1a6492fb88d6487826c
exen <kirill118reg@gmail.com>
2024-07-20 11:25:31 +0400

    Задание 4. Фразы с консоли

---
+++ Task4.Phrases/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task4.Phrases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your first phrase:");
+            string firstPhrase = Console.ReadLine();
+            Console.WriteLine("Please, enter your second phrase:");
+            string secondPhrase = Console.ReadLine();
+
+            Console.WriteLine("Your first phrase is " + firstPhrase +
+                " Your second phrase is " + secondPhrase);
+        }
+    }
+}
