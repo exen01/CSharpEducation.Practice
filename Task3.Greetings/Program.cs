75850c94b9b91e51e4b55fd3c4c9d5da814bb44d
exen <kirill118reg@gmail.com>
2024-07-20 11:25:06 +0400

    Задание 3. Приветствие

---
+++ Task3.Greetings/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task3.Greetings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
