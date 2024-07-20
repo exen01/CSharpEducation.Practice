40ea9e3c9439e1aa40a6c3ae3481196c6642aa36
exen <kirill118reg@gmail.com>
2024-07-20 11:25:55 +0400

    Задание 5. Площадь круга

---
+++ Task5.CircleArea/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task5.CircleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = Math.PI;
+            int radius = 10;
+
+            Console.WriteLine("Area of the circle is " + (pi * Math.Pow(radius, 2)));
+        }
+    }
+}
