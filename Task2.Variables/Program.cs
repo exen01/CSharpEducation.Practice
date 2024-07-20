689f8eece92c1ba1e6cbac8baa791ec4ff850d21
exen <kirill118reg@gmail.com>
2024-07-20 11:24:36 +0400

    Задание 2. Инициализация переменных

---
+++ Task2.Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2.Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 22;
+            string myName = "Kirill";
+            int temperature = 25;
+            bool isWoman = false;
+        }
+    }
+}
