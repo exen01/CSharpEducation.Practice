41c5df2389a25d7f37f12e8d99e46ab752620402
exen <kirill118reg@gmail.com>
2024-07-20 11:24:11 +0400

    Задание 1. Объявление переменных

---
+++ Task1.Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task1.Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            string companyName;
+            bool boolVar;
+            double weight;
+        }
+    }
+}
