2a39caeedd1ef19b12075c43dfd3679800b173ef
exen <kirill118reg@gmail.com>
2024-07-27 19:42:09 +0400

    Practice 2. Task 7

---
+++ Practice2.Task7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace Practice2.Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!".ToUpper());
+            Console.WriteLine("Hello, World!".ToLower());
+            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+            Console.WriteLine(ti.ToTitleCase("привет"));
+        }
+    }
+}
