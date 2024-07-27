5baaec0fda20e0cdc1343ffdc3ba118881e39f46
exen <kirill118reg@gmail.com>
2024-07-27 19:50:07 +0400

    Practice 2. Task 8

---
+++ Practice2.Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice2.Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if (a == b || a == c || b == c)
+            {
+                Console.WriteLine($"{a * 5} {b * 5} {c * 5}");
+            }
+            else
+            {
+                Console.WriteLine("равных нет");
+            }
+        }
+    }
+}
