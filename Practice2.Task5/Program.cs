00e5d38948b195be38428399f656359b358bd168
exen <kirill118reg@gmail.com>
2024-07-27 11:25:21 +0400

    Practice 2. Task 5

---
+++ Practice2.Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice2.Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int second = int.Parse(Console.ReadLine());
+
+            if (first == second)
+            {
+                Console.WriteLine(first + " is equal " + second);
+            }
+            else
+            {
+                Console.WriteLine(first + " is not equal " + second);
+            }
+        }
+    }
+}
