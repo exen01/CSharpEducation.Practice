9d4d0b1e401c37bfcc142895515fe317e2895d1e
exen <kirill118reg@gmail.com>
2024-07-27 10:19:08 +0400

    Practice 2. Task 1

---
+++ Practice2.Task1/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Practice2.Task1
+{
+    internal class Program
+    {
+        static void ForLoop()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void WhileLoop()
+        {
+            int whileCounter = 10;
+            while (whileCounter >= 0)
+            {
+                Console.WriteLine(whileCounter);
+                whileCounter--;
+            }
+        }
+
+        static void DoWhileLoop()
+        {
+            int whileCounter = 10;
+            do
+            {
+                Console.WriteLine(whileCounter);
+                whileCounter--;
+
+            } while (whileCounter >= 0);
+        }
+
+        static void PhrasesForLoop()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Enter any word: ");
+                string word = Console.ReadLine();
+                Console.WriteLine("You just enter " + word);
+            }
+        }
+
+        static void PhrasesWhileLoop()
+        {
+            int whileCounter = 10;
+            while (whileCounter >= 0)
+            {
+                Console.WriteLine("Enter any word: ");
+                string word = Console.ReadLine();
+                Console.WriteLine("You just enter " + word);
+                whileCounter--;
+            }
+        }
+
+        static void PhrasesDoWhileLoop()
+        {
+            int whileCounter = 10;
+            do
+            {
+                Console.WriteLine("Enter any word: ");
+                string word = Console.ReadLine();
+                Console.WriteLine("You just enter " + word);
+                whileCounter--;
+
+            } while (whileCounter >= 0);
+        }
+
+        static void Main(string[] args)
+        {
+            ForLoop();
+            WhileLoop();
+            DoWhileLoop();
+            PhrasesForLoop();
+            PhrasesWhileLoop();
+            PhrasesDoWhileLoop();
+        }
+    }
+}
