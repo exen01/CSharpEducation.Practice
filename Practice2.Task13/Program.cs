0bf85688f452307c6bf8c0254813f754ff0ae8dd
Kirill <kirill118reg@gmail.com>
2024-07-28 12:12:13 +0400

    Practice 2. Task 13

---
+++ Practice2.Task13/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Practice2.Task13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number of cols: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = CreateAndFillMatrix(rows, cols);
+
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int[,] CreateAndFillMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            Random rand = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rand.Next(100);
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
