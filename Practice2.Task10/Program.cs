edb50264d9d3e99660f994560d777f842f17e531
exen <kirill118reg@gmail.com>
2024-07-28 11:50:09 +0400

    Practice 2. Task 10

---
+++ Practice2.Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice2.Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(args[0]);
+            int[] array = new int[size];
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
