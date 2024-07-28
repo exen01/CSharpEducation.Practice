19e5bb18b4c853ecc49b7e066dd6da73de96f497
Kirill <kirill118reg@gmail.com>
2024-07-28 12:35:45 +0400

    Practice 2. Task 17

---
+++ Practice2.Task17/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice2.Task17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            Console.WriteLine("Before swap:");
+            Console.WriteLine($"num1: {num1}, num2: {num2}");
+
+            Swap(ref num1, ref num2);
+
+            Console.WriteLine("After swap:");
+            Console.WriteLine($"num1: {num1}, num2: {num2}");
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
