a73993434028c77f50a73009f8afa3be8575cba0
exen <kirill118reg@gmail.com>
2024-07-28 11:46:00 +0400

    Practice 2. Task 9

---
+++ Practice2.Task9/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice2.Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = CreateArray(3);
+            PrintArray(ints);
+        }
+
+        static int[] CreateArray(int size)
+        {
+            return new int[size];
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
