d5e9403c10a51d8d96cfdb4e25fc801bd8c5869f
Kirill <kirill118reg@gmail.com>
2024-07-28 12:21:39 +0400

    Practice 2. Task 16

---
+++ Practice2.Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice2.Task16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            ChangeSign(ref number);
+
+            Console.WriteLine(number);
+        }
+
+        static void ChangeSign(ref int num)
+        {
+            num = -num;
+        }
+    }
+}
