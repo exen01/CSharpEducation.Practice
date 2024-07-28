951e3f8f6d8757f707aa3345879ca77df081da62
Kirill <kirill118reg@gmail.com>
2024-07-28 12:19:06 +0400

    Practice 2. Task 15

---
+++ Practice2.Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice2.Task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3 };
+            InvertArray(ints);
+            foreach (var item in ints)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void InvertArray(int[] array)
+        {
+            Array.Reverse(array);
+        }
+    }
+}
