080f64373aa922f54185bf2f6c9b8f514b86f157
Kirill <kirill118reg@gmail.com>
2024-07-28 12:41:34 +0400

    Practice 2. Task 18

---
+++ Practice2.Task18/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Practice2.Task18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, -2, 3, -4, 5, -6, 7 };
+            int removed;
+
+            int[] modifiedArray = RemoveNegatives(numbers, out removed);
+
+            Console.WriteLine("array:");
+            Console.WriteLine(string.Join(", ", numbers));
+
+            Console.WriteLine("modifyed array:");
+            Console.WriteLine(string.Join(", ", modifiedArray));
+
+            Console.WriteLine($"removed number: {removed}");
+        }
+
+        static int[] RemoveNegatives(int[] inputArray, out int removedCount)
+        {
+            List<int> result = new List<int>();
+            removedCount = 0;
+
+            foreach (int num in inputArray)
+            {
+                if (num >= 0)
+                {
+                    result.Add(num);
+                }
+                else
+                {
+                    removedCount++;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
