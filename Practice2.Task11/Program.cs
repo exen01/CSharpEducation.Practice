1a3aec6310ee463f52bc8b7d4ef5a54ce49a6533
Kirill <kirill118reg@gmail.com>
2024-07-28 11:56:58 +0400

    Practice 2. Task 11

---
+++ Practice2.Task11/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice2.Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(args[0]);
+            int number = int.Parse(args[1]);
+
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = number;
+            }
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
