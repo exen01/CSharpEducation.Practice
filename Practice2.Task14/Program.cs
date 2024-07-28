a5307561cd5cd5ad66b2633e4aaec676212a4029
Kirill <kirill118reg@gmail.com>
2024-07-28 12:15:39 +0400

    Practice 2. Task 14

---
+++ Practice2.Task14/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice2.Task14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = CreateEmptyArray(5);
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(100);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static int[] CreateEmptyArray(int n)
+        {
+            return new int[n];
+        }
+    }
+}
