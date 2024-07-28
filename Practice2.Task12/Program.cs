ab825469367d6706a13c05c8c21c192e2eb0bf7d
Kirill <kirill118reg@gmail.com>
2024-07-28 12:00:39 +0400

    Practice 2. Task 12

---
+++ Practice2.Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice2.Task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[3];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter string:");
+                array[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Your array:");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
