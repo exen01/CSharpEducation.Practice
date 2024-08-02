d1122893bd8ac98be23f95e0858a85b00260d076
Kirill <kirill118reg@gmail.com>
2024-08-02 19:56:14 +0400

    Practice 2. Task 24

---
+++ Practice2.Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice2.Task24
+{
+    internal class Program
+    {
+        enum DaysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дни недели:");
+
+            foreach (string day in Enum.GetNames(typeof(DaysOfWeek)))
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
