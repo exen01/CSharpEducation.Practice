5ff81bde2ad072d11da489bb60672c79e9893864
Kirill <kirill118reg@gmail.com>
2024-08-02 19:22:08 +0400

    Practice 2. Task 21

---
+++ Practice2.Task21/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Practice2.Task21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int targetNumber = random.Next(1, 101);
+            int userGuess = 0;
+
+            Console.WriteLine("A number from 1 to 100 is guessed. Guess it!");
+
+            while (userGuess != targetNumber)
+            {
+                Console.Write("Введите ваше предположение: ");
+                userGuess = int.Parse(Console.ReadLine());
+
+                if (userGuess == targetNumber)
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                }
+            }
+        }
+    }
+}
