8b03014c10ced7b2f4e2d86933c9a6e0c05e2580
Kirill <kirill118reg@gmail.com>
2024-08-03 11:20:22 +0400

    Practice 3. Task 8

---
+++ Practice3/Practice3.Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice3.Task8
+{
+    internal class Program
+    {
+        struct Rectangle
+        {
+            public double width;
+            public double height;
+        }
+
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.width = 10;
+            rect.height = 5;
+            Console.WriteLine($"Площадь прямоугольника с шириной " +
+                $"{rect.width} и выслотой {rect.height} равна " +
+                CalculateRectangleArea(rect));
+        }
+
+        static double CalculateRectangleArea(Rectangle rect)
+        {
+            return rect.width * rect.height;
+        }
+    }
+}
