45dc9ded6723318b4131a9716719d0a003cd63b0
Kirill <kirill118reg@gmail.com>
2024-08-03 10:40:26 +0400

    Practice 3. Task 3

---
+++ Practice3/Practice3.Task3/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice3.Task3
+{
+    internal class Calculator
+    {
+        public int Addition(int a, int b) { return a + b; }
+        public int Subtraction(int a, int b) { return a - b; }
+        public double Division(int a, int b) { return (double)a / b; }
+        public int Multiplication(int a, int b) { return a * b; }
+    }
+}
---
+++ Practice3/Practice3.Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice3.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("1 + 3 = " + calculator.Addition(1, 3));
+            Console.WriteLine("1 - 3 = " + calculator.Subtraction(1, 3));
+            Console.WriteLine("1 * 3 = " + calculator.Multiplication(1, 3));
+            Console.WriteLine("1 / 3 = " + calculator.Division(1, 3));
+        }
+    }
+}
