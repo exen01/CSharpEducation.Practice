86a493b2123fd157db2451bc338697007a433114
Kirill <kirill118reg@gmail.com>
2024-08-03 16:49:25 +0400

    Practice 3. Task 11

---
+++ Practice3/Practice3.Task11/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice3.Task11
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+    }
+}
---
+++ Practice3/Practice3.Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice3.Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("New Book", "New Author");
+            PrintBook(book);
+        }
+
+        static void PrintBook(Book book)
+        {
+            Console.WriteLine($"Книга {book.Title}, автор {book.Author}");
+        }
+    }
+}
