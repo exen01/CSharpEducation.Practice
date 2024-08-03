0875382124b1aecf8ad55af7c1b276fb9c02d775
Kirill <kirill118reg@gmail.com>
2024-08-03 10:23:06 +0400

    Practice 3. Task 1

---
+++ Practice3/Practice3.Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice3.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Kirill", 22);
+            student.PrintStudentInfo();
+        }
+    }
+}
---
+++ Practice3/Practice3.Task1/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice3.Task1
+{
+    internal class Student
+    {
+        private string name;
+        private int age;
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void PrintStudentInfo()
+        {
+            Console.WriteLine($"Студент {this.name}, возраст {this.age}");
+        }
+    }
+}
