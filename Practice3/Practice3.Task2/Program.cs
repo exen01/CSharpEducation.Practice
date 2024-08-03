bd3e4d893ee1b3df4b4a7da5dc914d18e755e294
Kirill <kirill118reg@gmail.com>
2024-08-03 10:32:27 +0400

    Practice 3. Task 2

---
+++ Practice3/Practice3.Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice3.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Kirill", 22);
+            student.AvgScore = 5;
+            student.PrintStudent();
+        }
+    }
+}
---
+++ Practice3/Practice3.Task2/Student.cs
@@ -0,0 +1,36 @@
+﻿namespace Practice3.Task2
+{
+    internal class Student
+    {
+        private string name;
+        private int age;
+        private double avgScore;
+        public double AvgScore
+        {
+            get { return avgScore; }
+            set
+            {
+                if (value >= 0 && value <= 5)
+                {
+                    avgScore = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Средний балл должен быть в диапазоне от 1 до 5");
+                }
+            }
+        }
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void PrintStudent()
+        {
+            Console.WriteLine($"Студент {name}, возраст {age}, средний балл {avgScore}");
+        }
+
+    }
+}
