a76dea6e3e021e32b07c71e260879f93acf76085
Kirill <kirill118reg@gmail.com>
2024-08-03 10:54:39 +0400

    Practice 3. Task 5

---
+++ Practice3/Practice3.Task5/Book.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice3.Task4
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+        private int publishYear;
+
+        public Book()
+        {
+            title = "Some Title";
+            author = "Some Author";
+            publishYear = 10;
+        }
+
+        public Book(string title, string author, int publishYear) : this(title, author)
+        {
+            this.publishYear = publishYear;
+        }
+
+        public Book(string title, string author) : this(title)
+        {
+            this.author = author;
+        }
+
+        public Book(string title)
+        {
+            this.title = title;
+        }
+    }
+}
---
+++ Practice3/Practice3.Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿using Practice3.Task4;
+
+namespace Practice3.Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Book book1 = new Book("Title1", "Author1", 2000);
+            Book book2 = new Book("Title2", "Author2");
+            Book book3 = new Book("Title3");
+        }
+    }
+}
