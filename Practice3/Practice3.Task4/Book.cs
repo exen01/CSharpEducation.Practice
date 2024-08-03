c78f0831a561155d5da5fd85bd61303c364059ec
Kirill <kirill118reg@gmail.com>
2024-08-03 10:44:53 +0400

    Practice 3. Task 4

---
+++ Practice3/Practice3.Task4/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice3.Task4
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+
+        public Book()
+        {
+            title = "Some Title";
+            author = "Some Author";
+        }
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+    }
+}
---
+++ Practice3/Practice3.Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice3.Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Book book1 = new Book("New book", "New author");
+        }
+    }
+}
