a5565082b328266ce4f089fe81abeda16ab2facf
Kirill <kirill118reg@gmail.com>
2024-08-03 11:26:20 +0400

    Practice 3. Task 9

---
+++ Practice3/Practice3.Task9/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice3.Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Kirill", 22);
+            Console.WriteLine($"Студент {student.Name}");
+            ChangeName(student);
+            Console.WriteLine($"Студент {student.Name}");
+        }
+
+        static void ChangeName(Student student)
+        {
+            student.Name = "Аноним";
+        }
+    }
+}
---
+++ Practice3/Practice3.Task9/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice3.Task9
+{
+    internal class Student
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
