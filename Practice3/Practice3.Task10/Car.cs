c95735c5cf0701b64e65e67030c0294a3f0727b8
Kirill <kirill118reg@gmail.com>
2024-08-03 11:30:29 +0400

    Practice 3. Task 10

---
+++ Practice3/Practice3.Task10/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice3.Task10
+{
+    internal class Car
+    {
+        private string brand;
+        public string Brand { get { return brand; } }
+
+        public Car(string brand)
+        {
+            this.brand = brand;
+        }
+    }
+}
---
+++ Practice3/Practice3.Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice3.Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("BMW");
+            PrintCar(car);
+        }
+
+        static void PrintCar(Car car)
+        {
+            Console.WriteLine("Машина марки " + car.Brand);
+        }
+    }
+}
