66f345c3b933c26efb9fd1572e61544fbf9c0afa
Kirill <kirill118reg@gmail.com>
2024-08-13 20:47:15 +0400

    Practice 3. Part 2. Task 2 and Task 3

---
+++ Practice3/Practice3.2.Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice3._2.Task2
+{
+  using Practice3._2.Task1.MathLib;
+  using Practice3._2.Task3.PersonLib;
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      MathHelper mathHelper = new MathHelper();
+      int a = 5;
+      int b = 10;
+
+      Console.WriteLine(mathHelper.Addition(a, b));
+      Console.WriteLine(mathHelper.Subtraction(a, b));
+      Console.WriteLine(mathHelper.Division(a, b));
+      Console.WriteLine(mathHelper.Multiplication(a, b));
+
+      Person person = new Person("Kirill", 22);
+      person.Print();
+
+      Person person1 = new Person("Some name", 33);
+      person1.Print();
+    }
+  }
+}
---
+++ Practice3/Practice3.2.Task3.PersonLib/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice3._2.Task3.PersonLib
+{
+  public class Person
+  {
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+      Name = name;
+      Age = age;
+    }
+
+    public void Print()
+    {
+      Console.WriteLine($"Имя: {Name}, возраст: {Age}");
+    }
+  }
+}
