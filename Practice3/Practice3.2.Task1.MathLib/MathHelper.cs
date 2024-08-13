ab817f71f47c54507314fd16da4a60b586a1c8fa
Kirill <kirill118reg@gmail.com>
2024-08-13 20:46:24 +0400

    Practice 3. Part 2. Task 1

---
+++ Practice3/Practice3.2.Task1.MathLib/MathHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice3._2.Task1.MathLib
+{
+  public class MathHelper
+  {
+    public int Addition(int a, int b) { return a + b; }
+    public int Subtraction(int a, int b) { return a - b; }
+    public double Division(int a, int b) { return (double)a / b; }
+    public int Multiplication(int a, int b) { return a * b; }
+  }
+}
