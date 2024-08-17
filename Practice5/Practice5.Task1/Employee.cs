c1c88bf532b75af9fe203c830f61d4551395d630
Kirill <kirill118reg@gmail.com>
2024-08-17 10:32:33 +0400

    Practice 5. Task 1 B

--- Practice5/Practice5.Task1/Employee.cs
+++ Practice5/Practice5.Task1/Employee.cs
@@ -2,8 +2,14 @@
 {
   internal class Employee
   {
-    public string Name {  get; set; }
+    public string Name { get; set; }
     public int Salary { get; set; }
+
+    public Employee(string name, int salary)
+    {
+      Name = name;
+      Salary = salary;
+    }
 
     public virtual double CalculateBonus()
     {
--- Practice5/Practice5.Task1/Manager.cs
+++ Practice5/Practice5.Task1/Manager.cs
@@ -2,9 +2,26 @@
 {
   internal class Manager : Employee
   {
+    public Manager(string name, int salary, int teamSize) : base(name, salary)
+    {
+      TeamSize = teamSize;
+    }
+
+    public int TeamSize { get; set; }
+
     public override double CalculateBonus()
     {
-      return (Salary / 100) * 20;
+      double bonus = 0;
+      if (TeamSize > 5)
+      {
+        bonus = (Salary / 100) * (20 + 5);
+      }
+      else
+      {
+        bonus = (Salary / 100) * 20;
+      }
+
+      return bonus;
     }
   }
 }
