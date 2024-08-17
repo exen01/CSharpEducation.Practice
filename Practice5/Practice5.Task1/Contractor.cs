518c403b86652e5059a382ad36b378442e18b3ad
Kirill <kirill118reg@gmail.com>
2024-08-17 10:42:38 +0400

    Practice 5. Task 1 C

---
+++ Practice5/Practice5.Task1/Contractor.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice5.Task1
+{
+  internal class Contractor : Employee
+  {
+    public int HourlyRate { get; set; }
+    
+    public Contractor(string name, int salary, int hourlyRate) : base(name, salary)
+    {
+      HourlyRate = hourlyRate;
+    }
+
+    public new double CalculateBonus(int hoursWorked)
+    {
+      return Salary * hoursWorked;
+    }
+  }
+}
--- Practice5/Practice5.Task1/Program.cs
+++ Practice5/Practice5.Task1/Program.cs
@@ -4,15 +4,30 @@
   {
     static void Main(string[] args)
     {
-      Employee employee = new Employee();
-      employee.Name = "E1";
-      employee.Salary = 100;
-      Console.WriteLine("Employee salary: {0} bonus {1}", employee.Salary, employee.CalculateBonus());
+      Employee employee = new Employee("E1", 100);
+      //Console.WriteLine("Employee salary: {0} bonus {1}", employee.Salary, employee.CalculateBonus());
 
-      Manager manager = new Manager();
-      manager.Name = "M1";
-      manager.Salary = 200;
-      Console.WriteLine("Manager salary: {0} bonus {1}", manager.Salary, manager.CalculateBonus());
+      Manager manager = new Manager("M1", 100, 6);
+      //Console.WriteLine("Manager salary: {0} bonus {1}", manager.Salary, manager.CalculateBonus());
+
+      Contractor contractor = new Contractor("C1", 300, 2);
+
+      PrintBonus(employee);
+      PrintBonus(manager);
+      PrintBonus(contractor);
+    }
+
+    static void PrintBonus(Employee employee)
+    {
+      if (employee is Contractor)
+      {
+        Contractor contractor = (Contractor)employee;
+        Console.WriteLine(contractor.CalculateBonus(10));
+      }
+      else
+      {
+        Console.WriteLine(employee.CalculateBonus());
+      }
     }
   }
 }
