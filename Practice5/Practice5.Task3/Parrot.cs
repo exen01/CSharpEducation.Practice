53b80e51d319fe13deabce80871348d063fe4105
Kirill <kirill118reg@gmail.com>
2024-08-17 16:18:42 +0400

    Practice 5. Task 3 D

---
+++ Practice5/Practice5.Task3/Parrot.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice5.Task3
+{
+  internal class Parrot : Animal
+  {
+    public string Color { get; set; }
+
+    public Parrot(string name, int age, string color) : base(name, age)
+    {
+      Color = color;
+    }
+
+    public new void MakeSound()
+    {
+      Console.WriteLine("Parrot is talking");
+    }
+
+    public void MakeSound(string words)
+    {
+      Console.WriteLine(words);
+    }
+  }
+}
--- Practice5/Practice5.Task3/Program.cs
+++ Practice5/Practice5.Task3/Program.cs
@@ -4,13 +4,25 @@
   {
     static void Main(string[] args)
     {
-      Animal animal = new Animal();
-      Dog dog = new Dog();
-      Cat cat = new Cat();
+      Animal animal = new Animal("A1", 10);
+      Dog dog = new Dog("D1", 5);
+      Cat cat = new Cat("C1", 3);
+      Parrot parrot = new Parrot("P1", 2, "red");
 
-      animal.MakeSound();
-      dog.MakeSound();
-      cat.MakeSound();
+      List<Animal> animals = [animal, cat, parrot, dog];
+
+      foreach (var animalItem in animals)
+      {
+        if (animalItem is Parrot parrotItem)
+        {
+          parrot.MakeSound();
+          parrotItem.MakeSound("some word");
+        }
+        else
+        {
+          animalItem.MakeSound();
+        }
+      }
     }
   }
 }
