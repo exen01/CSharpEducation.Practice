2a9326a7991dc9af746762ec404afa1840b3c1a5
Kirill <kirill118reg@gmail.com>
2024-09-14 16:35:05 +0400

    Practice 7. Task 6

---
+++ Practice7/Practice7.Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice7.Task6;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Is Palindrome: {0}", "asadasd".IsPalindrome());
+    Console.WriteLine("Is Palindrome: {0}", "1234321".IsPalindrome());
+    Console.WriteLine("To title case: {0}", "hello world".ToTitleCase());
+  }
+}
